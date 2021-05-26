f5d8f854b9e7989dad35f4fcfc71438f50aaf78e
AndreaHarris <>
2021-05-26 22:02:21 +0000

    Big Steps

--- message.js
+++ message.js
@@ -1,5 +1,18 @@
+
 class Message {
-   // Write code here!
+  constructor(name , commands) {
+    
+    this.name = name;
+    if (!name) {
+      throw Error("Name required."); 
+    
+    this.commands = commands;
+    if(!commands) {
+      throw Error ("Commands required.")
+    }
+  }
+  }
 }
+
 
 module.exports = Message;
--- rover.js
+++ rover.js
@@ -1,5 +1,19 @@
 class Rover {
-   // Write code here!
-}
+   constructor(position, mode, generatorWatts) {
+   this.position = position;
+   if (!position) {
+     throw Error ("Position required.");
 
+   this.mode = mode;
+   if (!mode) {
+     throw Error("Mode required.");
+
+   this.generatorWatts = generatorWatts (110);
+   if(!generatorWatts) {
+     throw Error ("Generator watts required.");
+   }  
+   }  
+  }
+}
+}
 module.exports = Rover;
