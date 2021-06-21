e317fdd01a67ffe601a0c24b61111ce5cbbe2dae
AndreaHarris <83606648+AndreaHarr@users.noreply.github.com>
2021-06-21 05:48:51 +0000

    Having issues with line 14 in rover.js giving unexpected token

--- rover.js
+++ rover.js
@@ -1,6 +1,6 @@
 class Rover {
    constructor(position) {
-   this.position = position;
+   this.position = 123;
    
 
    this.mode = 'NORMAL';
@@ -11,11 +11,13 @@ class Rover {
   };  
 };  
   
-receiveMessage(messageObject) 
+receiveMessage(messageObject){ 
+  
   let messageName = messageObject.name;
   let returnObject = {};
   returnObject("message") = messageName
   let resultsArray = [];
+  
 
   for(let i = 0; i < commandObjectArray.length; i++){
     let commandObjectArray = commandObjectArray[i];
@@ -28,10 +30,10 @@ receiveMessage(messageObject)
         position:this.position,
         mode: this.mode,
         generatorWatts: this.generatorWatts
-      }
-    }
+      };
+    };
     resultsArray.push(commandObject);
-  }
+  };
     
     // you can use response or outputObject or anything you'd like for the variable name!
     //message: // you're responsible for the value of this property! This is what you're testing in your test
@@ -42,5 +44,5 @@ receiveMessage(messageObject)
   returnObject['results'] = resultsArray;
   returnObject //make sure to return your NEW OBJECT 
 
-
+};
 module.exports = Rover;
